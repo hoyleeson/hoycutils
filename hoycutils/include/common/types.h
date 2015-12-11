dce2596ef4b8cfce0aa4e4fab2f0e2412150eeaa
hoyleeson <hoyleeson@gmail.com>
2015-12-11 20:53:59 +0800

    add typecheck

--- hoycutils/include/common/types.h
+++ hoycutils/include/common/types.h
@@ -19,4 +19,26 @@ typedef int bool;
 #define  FALSE 	(0)
 #endif
 
+/*
+ * Check at compile time that something is of a particular type.
+ * Always evaluates to 1 so you may use it easily in comparisons.
+ */
+#define typecheck(type,x) \
+({	type __dummy; \
+	typeof(x) __dummy2; \
+	(void)(&__dummy == &__dummy2); \
+	1; \
+})
+
+/*
+ * Check at compile time that 'function' is a certain type, or is a pointer
+ * to that type (needs to use typedef for the function type.)
+ */
+#define typecheck_fn(type,function) \
+({	typeof(type) __tmp = function; \
+	(void)__tmp; \
+})
+
+
+
 #endif
