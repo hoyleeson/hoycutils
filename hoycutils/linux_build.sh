eeb9d590197f30649d01beeae6809e1995c869c6
hoyleeson <hoyleeson@gmail.com>
2016-02-17 10:52:02 +0800

    add missing file.

---
+++ hoycutils/linux_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INSTAL_DIR=$PWD/_install
+
+if [ -e $INSTAL_DIR ]; then
+	mkdir -p $INSTAL_DIR
+fi
+
+./configure --enable-debug --enable-verbose --prefix=$INSTAL_DIR 
+
+make
+make install
+
+#compile samples.
+cd ./samples
+make clean
+make
