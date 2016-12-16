ca0da7042a26562a853295606cf7561b8000bea3
hoyleeson <hoyleeson@gmail.com>
2016-12-16 15:03:23 +0800

    fix compile path

--- hoycutils/android_build.sh
+++ hoycutils/android_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NDK_DIR=/home/lixinhai/develop_tools/android_ndk/android-ndk-r10e
+NDK_DIR=/home/hoyleeson/develop_tools/android_ndk/android-ndk-r10e
 SYSROOT=$NDK_DIR/platforms/android-21/arch-arm/
 TOOLCHAIN=$NDK_DIR/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64
 
@@ -16,7 +16,7 @@ export CC="$TOOLCHAIN/bin/arm-linux-androideabi-gcc --sysroot=$SYSROOT"
 
 make
 
-/home/lixinhai/develop_tools/android_ndk/android-ndk-r10e/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc --sysroot=/home/lixinhai/develop_tools/android_ndk/android-ndk-r10e/platforms/android-21/arch-arm/ -shared  -fPIC -DPIC  client/.libs/client.o   common/libcommon.a -llog  --sysroot=/home/lixinhai/develop_tools/android_ndk/android-ndk-r10e/platforms/android-21/arch-arm/ -O2   -Wl,-soname -Wl,libclient.so -o client/.libs/libclient.so.0.0.0
+$NDK_DIR/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc --sysroot=$NDK_DIR/platforms/android-21/arch-arm/ -shared  -fPIC -DPIC  client/.libs/client.o   common/libcommon.a -llog  --sysroot=$NDK_DIR/platforms/android-21/arch-arm/ -O2   -Wl,-soname -Wl,libclient.so -o client/.libs/libclient.so.0.0.0
 
 make install
 
